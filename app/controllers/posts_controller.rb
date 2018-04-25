3767cfec82cf8bca96efff66497722809f58b0fa
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-25 02:17:50 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,7 @@ class PostsController < ApplicationController
 		post.title = params[:title]
 		post.description = params[:description]
 		post.save
+		puts post
 		redirect_to post_path(@post)
 	end
 end
